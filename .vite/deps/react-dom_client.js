e5ceb028aa0bf67eb91562a0699a08ac176e77cb
yadnyeshkolte <yadnyeshkolte@gmail.com>
2025-04-01 19:25:06 +0530

    update on removed the vite folder

--- .vite/deps/react-dom_client.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_client
-} from "./chunk-CU73NCLI.js";
-import "./chunk-7RFQQ6AE.js";
-import "./chunk-2GK3IQSO.js";
-import "./chunk-G3PMV62Z.js";
-export default require_client();
--- .vite/deps/react.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_react
-} from "./chunk-2GK3IQSO.js";
-import "./chunk-G3PMV62Z.js";
-export default require_react();
