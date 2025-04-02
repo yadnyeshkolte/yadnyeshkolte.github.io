66075ec1eb4723e359f8fdf46c696dce74c3b48d
yadnyeshkolte <yadnyeshkolte@gmail.com>
2025-04-02 19:00:03 +0530

    update on the live forgot to add the comment

--- src/main.jsx
+++ src/main.jsx
@@ -13,3 +13,4 @@ createRoot(document.getElementById('root')).render(
 //Uploading new Upload 28-Mar-2025
 //Uploading new Upload 01-Apr-2025 Performance Improvements
 //Uploading new Upload 01-Apr-2025 Main Thread Performance Improvements
+//Uploading new Upload 02-Apr-2025 Model Size reduced to 50kbs
